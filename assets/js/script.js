1cf5885c353bcc4f0ffc59ff8015f02cc074e3c8
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-08-18 12:28:37 -0500

    more foundation to work off of

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@
 //I need to make buttons that will start the quiz
 //I need to make buttons that will clear the highscore list and go back to the main quiz as well.
 //
+
+//will hold majority of the code
+const startQuiz = function(){
+
+}
+//will be used to receive the scores at the end.
+const getScore = function(){
+
+}
