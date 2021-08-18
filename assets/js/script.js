2a5ac4ba26006c1240b8ef8335578ba15d5a84a8
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-08-18 11:45:54 -0500

    comments in every file

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//Script will have all the functionality to the quiz itself
+//The quiz will have each answer have a certain value: I only need to make 2 values thankfully. One being correct and one being correct
+//I need to make buttons that will start the quiz
+//I need to make buttons that will clear the highscore list and go back to the main quiz as well.
+//
