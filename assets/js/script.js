2a2ba76e933d04a0a479cc9a4d03517fb7ca5fd8
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-08-20 17:14:44 -0500

    more starter functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,28 @@
 //I need to make buttons that will clear the highscore list and go back to the main quiz as well.
 //
 
+//this will hold the timer in the entire page.
+const timer = function(){
+
+}
+
+//this displays correct/incorrect when you click a specific answer on the quiz. 
+const answer = function(){
+  //display the word correct!
+  if (result === true){
+    console.log("Correct!");
+  } else {
+    console.log("Incorrect!");
+  }
+  
+}
+
+
+//this will make it so that the timer will reduce once an incorrect answer has been chosen.
+const scoreMinus = function(){
+
+}
+
 //will hold majority of the code
 const startQuiz = function(){
 
