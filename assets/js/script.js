2e8f93fedbc422cf790d7dcfd539b82fb05bd79c
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-08-21 18:14:59 -0500

    working timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
+const timerEl = document.getElementById('timer');
 //Script will have all the functionality to the quiz itself
 //The quiz will have each answer have a certain value: I only need to make 2 values thankfully. One being correct and one being correct
 //I need to make buttons that will start the quiz
 //I need to make buttons that will clear the highscore list and go back to the main quiz as well.
 //
 
-//this will hold the timer in the entire page.
-const timer = function(){
-
+//score is determined by the remaining time on the timer.
+const score = function(){
 }
+//this will hold the timer in the entire page.
+var count = 11; 
+var counter = setInterval(timer, 1000);
+const timer = function(){
+  count = count-1;
+  if (count <= -1) {
+    clearInterval(counter);
+    return;
+  }
+  document.getElementById("timer").innerHTML=count;
+}
+
+
 
 //this displays correct/incorrect when you click a specific answer on the quiz. 
 const answer = function(){
@@ -20,6 +33,9 @@ const answer = function(){
   
 }
 
+const saveScore = function(){
+  localStorage.setItem("score", score);
+}
 
 //this will make it so that the timer will reduce once an incorrect answer has been chosen.
 const scoreMinus = function(){
