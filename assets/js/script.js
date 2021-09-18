614817eeae6cac1cc3af24e923f30a637818bfce
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-18 18:13:25 -0500

    default question template for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const timerEl = document.getElementById('timer');
+const timerEl = document.getElementById("timer");
 //Script will have all the functionality to the quiz itself
 //The quiz will have each answer have a certain value: I only need to make 2 values thankfully. One being correct and one being correct
 //I need to make buttons that will start the quiz
@@ -6,52 +6,64 @@ const timerEl = document.getElementById('timer');
 //
 
 //score is determined by the remaining time on the timer.
-const score = function(){
-}
+const score = function () {};
 //this will hold the timer in the entire page.
-var count = 11; 
-const timer = function(){
-  count = count-1;
+var count = 11;
+const timer = function () {
+  count = count - 1;
   if (count <= -1) {
     clearInterval(counter);
     return;
   }
-  document.getElementById("timer").innerHTML="Time: " + count;
-  
-}
+  document.getElementById("timer").innerHTML = "Time: " + count;
+};
 
 var counter = setInterval(timer, 1000);
 
-//this displays correct/incorrect when you click a specific answer on the quiz. 
-const answer = function(){
+//this displays correct/incorrect when you click a specific answer on the quiz.
+const answer = function () {
   //display the word correct!
-  if (result === true){
+  if (result === true) {
     console.log("Correct!");
   } else {
     console.log("Incorrect!");
   }
-  
-}
+};
 
-const saveScore = function(){
+const saveScore = function () {
   localStorage.setItem("score", score);
-}
+};
 
 //this will make it so that the timer will reduce once an incorrect answer has been chosen.
-const scoreMinus = function(){
-
-}
+const scoreMinus = function () {};
 
 //will hold majority of the code
-const startQuiz = function(){
-object.addEventListener("click", startQuiz);
-}
+const startQuiz = function () {
+  object.addEventListener("click", startQuiz);
+};
 //will be used to receive the scores at the end.
-const getScore = function(){
-
-}
+const getScore = function () {};
 
 
-const Q1 = [
-
-]
+const questions = [
+  {
+    question: "What color is the sky",
+    answers: ["red", "yellow", "green", "blue"],
+    correctAnswer: "blue",
+  },
+  {
+    question: "What color is the sky",
+    answers: ["red", "yellow", "green", "blue"],
+    correctAnswer: "blue",
+  },
+  {
+    question: "What color is the sky",
+    answers: ["red", "yellow", "green", "blue"],
+    correctAnswer: "blue",
+  },
+  {
+    question: "What color is the sky",
+    answers: ["red", "yellow", "green", "blue"],
+    correctAnswer: "blue",
+  },
+];
