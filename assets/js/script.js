9ac182eff5481d2adb8f8688928871ae40910d27
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-18 18:23:30 -0500

    create quiz event listener, update startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,6 @@
 const timerEl = document.getElementById("timer");
-//Script will have all the functionality to the quiz itself
-//The quiz will have each answer have a certain value: I only need to make 2 values thankfully. One being correct and one being correct
-//I need to make buttons that will start the quiz
-//I need to make buttons that will clear the highscore list and go back to the main quiz as well.
-//
-
-//score is determined by the remaining time on the timer.
-const score = function () {};
-//this will hold the timer in the entire page.
-var count = 11;
-const timer = function () {
-  count = count - 1;
-  if (count <= -1) {
-    clearInterval(counter);
-    return;
-  }
-  document.getElementById("timer").innerHTML = "Time: " + count;
-};
-
-var counter = setInterval(timer, 1000);
-
-//this displays correct/incorrect when you click a specific answer on the quiz.
-const answer = function () {
-  //display the word correct!
-  if (result === true) {
-    console.log("Correct!");
-  } else {
-    console.log("Incorrect!");
-  }
-};
-
-const saveScore = function () {
-  localStorage.setItem("score", score);
-};
-
-//this will make it so that the timer will reduce once an incorrect answer has been chosen.
-const scoreMinus = function () {};
-
-//will hold majority of the code
-const startQuiz = function () {
-  object.addEventListener("click", startQuiz);
-};
-//will be used to receive the scores at the end.
-const getScore = function () {};
-
+const answerEl = document.getElementById("answers");
+const quizEl = document.getElementById("quiz");
 
 const questions = [
   {
@@ -67,3 +24,59 @@ const questions = [
     correctAnswer: "blue",
   },
 ];
+//Script will have all the functionality to the quiz itself
+//The quiz will have each answer have a certain value: I only need to make 2 values thankfully. One being correct and one being correct
+//I need to make buttons that will start the quiz
+//I need to make buttons that will clear the highscore list and go back to the main quiz as well.
+//
+
+//score is determined by the remaining time on the timer.
+const score = function () {};
+//this will hold the timer in the entire page.
+var count = 11;
+const timer = function () {
+  count = count - 1;
+  if (count <= -1) {
+    clearInterval(counter);
+    return;
+  }
+  timerEl.innerHTML = "Time: " + count;
+};
+
+var counter = setInterval(timer, 1000);
+
+//this displays correct/incorrect when you click a specific answer on the quiz.
+const answer = function () {
+  //display the word correct!
+  if (result === true) {
+    console.log("Correct!");
+  } else {
+    console.log("Incorrect!");
+  }
+};
+
+const saveScore = function () {
+  localStorage.setItem("score", score);
+};
+
+//this will make it so that the timer will reduce once an incorrect answer has been chosen.
+const scoreMinus = function () {};
+
+//will call timer and quiz questions
+const startQuiz = function () {
+ timer();
+ createQuestion();
+};
+
+//will be used to receive the scores at the end.
+const getScore = function () {};
+
+const createQuestion = function(){
+
+}
+
+
+
+
+
+quizEl.addEventListener("click", startQuiz);
